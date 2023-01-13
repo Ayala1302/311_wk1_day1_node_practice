f13c92e54325d516763c998950a3a8186d556eb9
Ayala1302 <marcoayala48@yahoo.com>
2023-01-12 18:04:26 -0600

    finished

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,20 +1,23 @@
 const split = (str, delim) => {
   // write code for strings.split
-
-}
+  return str.split(delim);
+};
 
 const pairs = (str) => {
   // write code for strings.pairs
-
-}
-
+  let arr = [];
+  for (let i = 0; i < str.length; i += 2) {
+    arr.push(str.slice(i, i + 2));
+  }
+  return arr;
+};
 const reverse = (str) => {
   // write code for strings.reverse
-
-}
+  return str.split("").reverse().join("");
+};
 
 module.exports = {
   split,
   pairs,
-  reverse
-}
+  reverse,
+};
